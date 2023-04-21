908bc3d406179afac76a8b64ee720ac44a93f093
lahatra3 <lahatrap20.aps2a@gmail.com>
2023-04-21 19:24:31 +0300

    [UPDATE] update listes...

--- listes.cs
+++ listes.cs
@@ -3,6 +3,24 @@ using System.Data.SqlClient;
 class Listes {
     public Listes() {}
 
+    public void create(string nom) {
+        SqlConnection connection =  Database.db_connection();
+        string query = "INSERT INTO listes(nom) VALUES(@Nom)";
+
+        try {
+            connection.Open();
+            SqlCommand command = new SqlCommand(query, connection);
+            command.Parameters.AddWithValue("@Nom", nom);
+            command.ExecuteNonQuery();
+        }
+        catch(Exception e) {
+            Console.WriteLine("Erreur, connexion à la base de données !\n" + e.Message);
+        }
+        finally {
+            connection.Close();
+        }
+    }
+
     public List<Dictionary<string, object>> findall() {
         SqlConnection connection =  Database.db_connection();
         string query = "SELECT * FROM listes";
@@ -28,5 +46,42 @@ class Listes {
         }
 
         return results;
+    }
+
+    public void update(int liste_id, string nom) {
+        SqlConnection connection =  Database.db_connection();
+        string query = "UPDATE listes SET nom = @Nom WHERE id = @ListeId";
+
+        try {
+            connection.Open();
+            SqlCommand command = new SqlCommand(query, connection);
+            command.Parameters.AddWithValue("@ListeId", liste_id);
+            command.Parameters.AddWithValue("@Nom", nom);
+            command.ExecuteNonQuery();
+        }
+        catch(Exception e) {
+            Console.WriteLine("Erreur, connexion à la base de données !\n" + e.Message);
+        }
+        finally {
+            connection.Close();
+        }
+    }
+
+    public void delete(int liste_id) {
+        SqlConnection connection =  Database.db_connection();
+        string query = "DELETE FROM listes WHERE id = @ListeId";
+
+        try {
+            connection.Open();
+            SqlCommand command = new SqlCommand(query, connection);
+            command.Parameters.AddWithValue("@ListeId", liste_id);
+            command.ExecuteNonQuery();
+        }
+        catch(Exception e) {
+            Console.WriteLine("Erreur, connexion à la base de données !\n" + e.Message);
+        }
+        finally {
+            connection.Close();
+        }
     }
 }
